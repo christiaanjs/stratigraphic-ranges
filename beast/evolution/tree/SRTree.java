7b3ff58d2ebfe8c8d18be32369c6751a3fb88a6a
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2018-09-05 13:59:30 +0200

    Refactor SRTree to use more inherited code

--- beast/evolution/tree/SRTree.java
+++ beast/evolution/tree/SRTree.java
@@ -162,20 +162,7 @@ public class SRTree extends Tree {
      */
     @Override
     public void assignFrom(final StateNode other) {
-        final Tree tree = (Tree) other;
-        final Node[] nodes = new Node[tree.getNodeCount()];//tree.getNodesAsArray();
-        for (int i = 0; i < tree.getNodeCount(); i++) {
-            nodes[i] = newNode();
-        }
-        setID(tree.getID());
-        //index = tree.index;
-        root = nodes[tree.root.getNr()];
-        root.assignFrom(nodes, tree.root);
-        root.parent = null;
-        nodeCount = tree.nodeCount;
-        internalNodeCount = tree.internalNodeCount;
-        leafNodeCount = tree.leafNodeCount;
-        initArrays();
+        super.assignFrom(other);
         initSRanges();
     }
 
@@ -184,54 +171,12 @@ public class SRTree extends Tree {
      */
     @Override
     public void assignFromFragile(final StateNode other) {
-        // invalidate cache
-        postCache = null;
-
-        final Tree tree = (Tree) other;
-        if (m_nodes == null) {
-            initArrays();
-        }
-        root = m_nodes[tree.root.getNr()];
-        final Node[] otherNodes = tree.m_nodes;
-        final int rootNr = root.getNr();
-        assignFrom(0, rootNr, otherNodes);
-        root.height = otherNodes[rootNr].height;
-        root.parent = null;
-        if (otherNodes[rootNr].getLeft() != null) {
-            root.setLeft(m_nodes[otherNodes[rootNr].getLeft().getNr()]);
-        } else {
-            root.setLeft(null);
-        }
-        if (otherNodes[rootNr].getRight() != null) {
-            root.setRight(m_nodes[otherNodes[rootNr].getRight().getNr()]);
-        } else {
-            root.setRight(null);
-        }
-        assignFrom(rootNr + 1, nodeCount, otherNodes);
+        super.assignFromFragile(other);
         if(stratigraphicRangeInput.get()!= null) {
             initSRanges();
         }
     }
 
-    /**
-     * helper to assignFromFragile *
-     */
-    private void assignFrom(final int start, final int end, final Node[] otherNodes) {
-        for (int i = start; i < end; i++) {
-            Node sink = m_nodes[i];
-            Node src = otherNodes[i];
-            sink.height = src.height;
-            sink.parent = m_nodes[src.parent.getNr()];
-            if (src.getLeft() != null) {
-                sink.setLeft(m_nodes[src.getLeft().getNr()]);
-                if (src.getRight() != null) {
-                    sink.setRight(m_nodes[src.getRight().getNr()]);
-                } else {
-                    sink.setRight(null);
-                }
-            }
-        }
-    }
 
     /**
      * StateNode implementation *
@@ -239,8 +184,8 @@ public class SRTree extends Tree {
     @Override
     protected void store() {
 
-        storeNodes(0, nodeCount);
-        storedRoot = m_storedNodes[root.getNr()];
+        super.store();
+
         for (StratigraphicRange range_src:sRanges) {
             int index = sRanges.indexOf(range_src);
             StratigraphicRange range_sink = storedSRanges.get(index);
@@ -252,81 +197,10 @@ public class SRTree extends Tree {
         }
     }
 
-    /**
-     * Stores nodes with index i, for start <= i < end
-     * (i.e. including start but not including end)
-     *
-     * @param start the first index to be stored
-     * @param end   nodes are stored up to but not including this index
-     */
-    private void storeNodes(final int start, final int end) {
-        // Use direct members for speed (we are talking 5-7% or more from total time for large trees :)
-        for (int i = start; i < end; i++) {
-            final SRNode sink = (SRNode)m_storedNodes[i];
-            final SRNode src = (SRNode)m_nodes[i];
-            sink.height = src.height;
-
-            if ( src.parent != null ) {
-                sink.parent = m_storedNodes[src.parent.getNr()];
-            } else {
-                // currently only called in the case of sampled ancestor trees
-                // where root node is not always last in the list
-                sink.parent = null;
-            }
-
-            final List<Node> children = sink.children;
-            final List<Node> srcChildren = src.children;
-
-            if( children.size() == srcChildren.size() ) {
-                // shave some more time by avoiding list clear and add
-                for (int k = 0; k < children.size(); ++k) {
-                    final SRNode srcChild = (SRNode)srcChildren.get(k);
-                    // don't call addChild, which calls  setParent(..., true);
-                    final Node c = m_storedNodes[srcChild.getNr()];
-                    c.parent = sink;
-                    children.set(k, c);
-                }
-            } else {
-                children.clear();
-                //sink.removeAllChildren(false);
-                for (final Node srcChild : srcChildren) {
-                    // don't call addChild, which calls  setParent(..., true);
-                    final Node c = m_storedNodes[srcChild.getNr()];
-                    c.parent = sink;
-                    children.add(c);
-                    //sink.addChild(c);
-                }
-            }
-        }
-    }
-
     @Override
     public void restore() {
 
-        // necessary for sampled ancestor trees
-        nodeCount = m_storedNodes.length;
-
-        final Node[] tmp = m_storedNodes;
-        m_storedNodes = m_nodes;
-        m_nodes = tmp;
-        root = m_nodes[storedRoot.getNr()];
-
-        // necessary for sampled ancestor trees,
-        // we have the nodes, no need for expensive recursion
-        leafNodeCount = 0;
-        for( Node n : m_nodes ) {
-            leafNodeCount += n.isLeaf() ? 1 : 0;
-        }
-
-        //leafNodeCount = root.getLeafNodeCount();
-
-        hasStartedEditing = false;
-
-        for( Node n : m_nodes ) {
-            n.isDirty = Tree.IS_CLEAN;
-        }
-
-        postCache = null;
+        super.restore();
 
         ArrayList<StratigraphicRange> tmp_ranges = storedSRanges;
         storedSRanges = sRanges;
